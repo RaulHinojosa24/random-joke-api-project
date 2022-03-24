ad8f3cf18f8dbbde5d5386d5b1e57de50fa37453
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-03-24 18:54:43 +0100

    solucionado

--- app.js
+++ app.js
@@ -2,3 +2,12 @@
 const image = document.querySelector('img');
 const jokeDIV = document.querySelector('#display-joke');
 const button = document.querySelector('#get-joke');
+
+button.addEventListener("click", getJoke);
+
+function getJoke() {
+    fetch('https://api.chucknorris.io/jokes/random')
+        .then(response => response.json())
+        .then(data => jokeDIV.textContent = data.value)
+        .catch(error => jokeDIV.textContent = "Ha habido un error al solicitar la información.");
+}
--- app_fetch.js
+++ app_fetch.js
@@ -3,17 +3,17 @@ const image = document.querySelector('img');
 const jokeDIV = document.querySelector('#display-joke');
 const button = document.querySelector('#get-joke');
 
-button.addEventListener('click', function(){
+button.addEventListener('click', function () {
     getRandomJoke();
 })
 
-function getRandomJoke(){
+function getRandomJoke() {
     const url = 'https://api.chucknorris.io/jokes/random'
- 
+
     fetch(url).
-    then(response => response.json()).
-    then(data => updateDOM(data)).
-    catch(error => console.log(error))
+        then(response => response.json()).
+        then(data => updateDOM(data)).
+        catch(error => console.log(error))
 
     // Fijaos que esto se ejecuta antes de recibir la respuesta del fetch!!
     console.log(jokeDIV.textContent)
@@ -24,6 +24,6 @@ function updateDOM(data) {
     jokeDIV.innerHTML = data.value
 }
 
-function onerror(){
-     jokeDIV.textContent = 'There was an error!';  
+function onerror() {
+    jokeDIV.textContent = 'There was an error!';
 }
